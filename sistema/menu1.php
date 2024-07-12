d0804a89e694bbf54a93df1201791e29be629753
iBerserker89 <lucianof.barros89@gmail.com>
2024-07-12 19:13:54 -0300

    enviando layout

---
+++ sistema/menu1.php
@@ -0,0 +1,14 @@
+<div id="site">
+    <div id="sombra">
+        <div id="topo">
+            <div id="nome-do-site">Site em PHP</div>
+        </div>
+        <div id="main-wrapper">
+            <div id="menu">
+                <ul>
+                    <li><a href="#">item 1</a></li>
+                    <li><a href="#">item 2</a></li>
+                    <li><a href="#">item 3</a></li>
+                    <li><a href="#">item 4</a></li>
+                </ul>
+            </div>
---
+++ sistema/menu2.php
@@ -0,0 +1,8 @@
+<div id="conteudo">
+</div>
+</div>
+</div>
+</div>
+<div id="rodape">
+    Site criado no curso de PHP para iniciantes da <a href="http://www.iparos.com.br" target="_blank">Íparos</a>
+</div>
